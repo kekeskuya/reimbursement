0d82241ce469b312b1515422b5fa504b88e7381c
kekeskuya <59590417+kekeskuya@users.noreply.github.com>
2023-01-25 05:58:09 +0700

    Add files via upload

---
+++ admin/reimburse_page.php
@@ -0,0 +1,89 @@
+<?php
+session_start();
+/**
+ * Jika Tidak login atau sudah login tapi bukan sebagai admin
+ * maka akan dibawa kembali kehalaman login atau menuju halaman yang seharusnya.
+ */
+/* 
+if ( !isset($_SESSION['user_login']) || 
+    ( isset($_SESSION['user_login']) && $_SESSION['user_login'] != 'member' ) ) {
+
+	header('location:./../login.php');
+	exit();
+}
+*/
+?>
+<!--
+<h2>Hallo Member <?=$_SESSION['nama_user'];?> Apakabar ?</h2>
+
+<a href="./../logout.php">Logout</a>
+-->
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="reimburse_page.php">Reimburse</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link disabled">Disabled</a>
+      </li>
+    </ul>
+	
+    <form class="form-inline my-2 my-lg-0">
+      <p class="text-left"><h6> <?=$_SESSION['nama_user'];?></h6></p>
+    </form>
+  </div>
+</nav>
+
+<div class="row"><br/></div>
+
+<!-- Button trigger modal -->
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
